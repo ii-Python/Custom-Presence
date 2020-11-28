d18ff5c11dcdd76214ad8877c8e00c3b7fc062b1
Benjamin O'Brien <bobrien9274@gmail.com>
2020-11-27 23:20:15 -0600

    Remove termcolor as a dependency

--- presence/colors.py
+++ presence/colors.py
@@ -1,1 +1,14 @@
-from termcolor import colored
+# Initialization
+colors = {
+  "red": "\033[91m",
+  "green": "\033[92m",
+  "cyan": "\033[36m",
+  "blue": "\033[94m",
+  "yellow": "\033[93m",
+  "reset": "\033[0m"
+}
+
+# Colored function
+def colored(text, color):
+
+    return colors[color] + text + colors["reset"]
