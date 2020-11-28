0f51b4af43097a358fa52d40b07ed996c62dfcc0
Benjamin O'Brien <bobrien9274@gmail.com>
2020-11-28 10:25:02 -0600

    Bump to 1.0.7

--- presence/__init__.py
+++ presence/__init__.py
@@ -3,5 +3,5 @@ from .core import rpc as RPC
 from .logging import crash, info
 
 __author__ = "Benjamin O'Brien (iiPython)"
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 __copyright__ = f"Copyright 2020; {__author__}"
