7450c8e0fcf36a394c5cf472707676ac1139cd4b
Benjamin O'Brien <bobrien9274@gmail.com>
2020-12-01 20:41:22 -0600

    Bump version

--- presence/__init__.py
+++ presence/__init__.py
@@ -4,5 +4,5 @@ from .colors import colored, clear
 from .scripts.cmd import run_commands
 
 __author__ = "Benjamin O'Brien (iiPython)"
-__version__ = "1.0.9"
+__version__ = "1.1.0-alpha"
 __copyright__ = f"Copyright 2020; {__author__}"
